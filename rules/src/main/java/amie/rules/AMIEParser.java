74cd28330896cdd197a298fc9ec5b25aedfdf3ce
Antonio Carlos <falcaopetri@gmail.com>
2020-06-05 11:28:19 -0300

    [AMIEParser] Reformatted file

--- rules/src/main/java/amie/rules/AMIEParser.java
+++ rules/src/main/java/amie/rules/AMIEParser.java
@@ -8,82 +8,78 @@ import java.util.List;
 import amie.data.KB;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
-import it.unimi.dsi.fastutil.ints.IntArrayList;
-import it.unimi.dsi.fastutil.ints.IntList;
-
 
 import javatools.datatypes.Pair;
-import javatools.filehandlers.FileLines;
 import javatools.filehandlers.TSVFile;
 
-/** 
+/**
  * Parses a file of AMIE rules
- * 
+ *
  * @author Fabian
  *
  */
 public class AMIEParser {
-	
-	/**
-	 * Parsers an AMIE rule from a string.
-	 * @param s
-	 * @return
-	 */
-	public static Rule rule(String s) {
-		Pair<List<int[]>, int[]> rulePair = KB.rule(s.trim());
-		if(rulePair == null) return null;
-		Rule resultRule = new Rule(rulePair.second, rulePair.first, 0);
-		resultRule.setGeneration(resultRule.getLength());
-		return resultRule;
-	}
 
-	public static Rule rule(List<String> record) {
-		Rule rule = rule(record.get(0));
-		if (rule == null)
-			return null;
+    /**
+     * Parsers an AMIE rule from a string.
+     * @param s
+     * @return
+     */
+    public static Rule rule(String s) {
+        Pair<List<int[]>, int[]> rulePair = KB.rule(s.trim());
+        if(rulePair == null) return null;
+        Rule resultRule = new Rule(rulePair.second, rulePair.first, 0);
+        resultRule.setGeneration(resultRule.getLength());
+        return resultRule;
+    }
 
-		if (record.size() > 1) {
-			rule.setSupport(Double.parseDouble(record.get(4)));
-			rule.setBodySize(Long.parseLong(record.get(5)));
-			rule.setPcaBodySize(Double.parseDouble(record.get(6)));
-		}
+    public static Rule rule(List<String> record) {
+        Rule rule = rule(record.get(0));
+        if (rule == null)
+            return null;
 
-		return rule;
-	}
-  
-	public static void normalizeRule(Rule q){
-		char c = 'a';
-		Int2ObjectMap<Character> charmap = new Int2ObjectOpenHashMap<Character>();
-		for(int[] triple: q.getTriples()){
-			for(int i = 0;  i < triple.length; ++i){
-				if(KB.isVariable(triple[i])){
-					Character replace = charmap.get(triple[i]);
-					if(replace == null){
-						replace = new Character(c);
-						charmap.put(triple[i], replace);
-						c = (char) (c + 1);
-					}
-					triple[i] = KB.map("?" + replace);				
-				}
-			}
-		}
-	}	
+        if (record.size() > 1) {
+            rule.setSupport(Double.parseDouble(record.get(4)));
+            rule.setBodySize(Long.parseLong(record.get(5)));
+            rule.setPcaBodySize(Double.parseDouble(record.get(6)));
+        }
 
-	public static List<Rule> rules(File f) throws IOException {
-	    TSVFile tsvFile = new TSVFile((f));
-	    List<Rule> result = new ArrayList<>();
-	    for(List<String> record : tsvFile) {
-	        Rule q = AMIEParser.rule(record);
+        return rule;
+    }
 
-	        if (q == null)
-	            continue;
+    public static void normalizeRule(Rule q){
+        char c = 'a';
+        Int2ObjectMap<Character> charmap = new Int2ObjectOpenHashMap<Character>();
+        for(int[] triple: q.getTriples()){
+            for(int i = 0;  i < triple.length; ++i){
+                if(KB.isVariable(triple[i])){
+                    Character replace = charmap.get(triple[i]);
+                    if(replace == null){
+                        replace = new Character(c);
+                        charmap.put(triple[i], replace);
+                        c = (char) (c + 1);
+                    }
+                    triple[i] = KB.map("?" + replace);
+                }
+            }
+        }
+    }
 
-	        result.add(q);
-	    }
-	    return(result);
-	}
-	  
-	public static void main(String[] args) throws Exception {
-	    System.out.println(AMIEParser.rule("=> ?a <hasChild> ?b"));
-	}
+    public static List<Rule> rules(File f) throws IOException {
+        TSVFile tsvFile = new TSVFile((f));
+        List<Rule> result = new ArrayList<>();
+        for(List<String> record : tsvFile) {
+            Rule q = AMIEParser.rule(record);
+
+            if (q == null)
+                continue;
+
+            result.add(q);
+        }
+        return(result);
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(AMIEParser.rule("=> ?a <hasChild> ?b"));
+    }
 }
